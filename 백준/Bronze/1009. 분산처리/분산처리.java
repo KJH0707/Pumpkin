1d99ed931abc5b97342c50d3710931021323a89d
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2022-10-03 18:51:03 +0900

    [Bronze II] Title: 분산처리, Time: 280 ms, Memory: 18580 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.java
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -10,14 +10,25 @@ public class Main {
 		for(int i=0;i<T;i++) {
 			int num = in.nextInt();
 			int squared = in.nextInt();
+			int T2=1;
 			int fNum=1;
-			for (int j=0;j<squared;j++) {
+			
+			if (num%10==1 | num%10==0 | num%10==5 | num%10==6) {
+				fNum=num%10;
+			}
+			else if (num%10==4|num%10==9) {
+				T2=squared%2;
+				if (T2==0) T2=2;
+			}
+			else {
+				T2=squared%4;
+				if (T2==0) T2=4;
+			}
+			for (int j=0;j<T2;j++) {
 				fNum*=num;
 				fNum%=10;
-				if (fNum==0) {
-					fNum=10;
-				}
 			}
+			if (fNum==0) fNum=10;
 			System.out.println(fNum);
 		}
 		in.close();
