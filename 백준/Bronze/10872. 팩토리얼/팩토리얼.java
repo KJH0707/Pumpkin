12be673a0a54064b4f4827e5eb2d67cfa7feeca5
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2022-10-10 18:11:48 +0900

    [Bronze V] Title: 팩토리얼, Time: 204 ms, Memory: 17580 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        int a = in.nextInt();
-        int sum=1;
-        
-        for (int i=1;i<=a;i++) {
-        	sum=sum*i;
-        	
-        	}
-        System.out.println(sum);
-    }
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println(method1(in.nextInt()));
+		
+	}
+	static int method1(int a) {
+		if (a<=1) return 1;
+		return a * method1(a-1);
+	}
 }
