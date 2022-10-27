672a0b0199dd4f6143474db51c47505807484eca
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2022-10-27 23:35:11 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 212 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int a;
+		int b;
+		
+		while(true){
+			a = in.nextInt();
+			b = in.nextInt();
+			if (a==0 & b==0) {
+				break;
+			}
+			
+			System.out.println(a+b);
+		}
+		
+	}
+
+}
