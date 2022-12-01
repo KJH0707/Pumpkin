18030f096f2139eb61e1e4550fd1a5ddeb2a5989
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2022-12-01 16:31:46 +0900

    [Bronze IV] Title: Hello Judge, Time: 228 ms, Memory: 18224 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int T = in.nextInt();
+        
+        for (int i=1;i<=T;i++) {
+            System.out.println("Hello World, Judge "+i+"!");
+        }
+    }
+    
+}
