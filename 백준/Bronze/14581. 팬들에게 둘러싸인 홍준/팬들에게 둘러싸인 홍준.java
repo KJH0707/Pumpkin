42ffda0447000569e612bd356e00f2e58c714b31
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2022-12-05 23:24:14 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 216 ms, Memory: 18244 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		String a = in.next();
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::"+a+"::fan:");
+		System.out.println(":fan::fan::fan:");
+		
+	}
+
+}
