7635021c1069186663135b022d77f704c8ed0d07
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2023-04-20 22:56:39 +0900

    [Bronze II] Title: 소수, Time: 352 ms, Memory: 14612 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        int total = 0;
+        int min = 10000;
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i=a;i<=b;i++) {
+            int cnt = 0;
+            for (int j=1;j<=i;j++) {
+                if (i%j==0) cnt++;
+            }
+            if (cnt==2) {
+                total+=i;
+                if(min>i) min=i;
+            }
+        }
+        if (total==0) {
+            System.out.println(-1);
+        }else {
+            System.out.println(total);
+            System.out.println(min);
+        }
+    }
+}
