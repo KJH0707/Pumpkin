0e895c2b255439398bae2f7873811e7cb58a8680
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2023-04-24 21:09:41 +0900

    [Bronze I] Title: 소인수분해, Time: 164 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -8,21 +8,17 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<Integer>();
         int cnt=2;
 
         while(a!=1) {
             if(a%cnt==0) {
                 a/=cnt;
-                list.add(cnt);
+                System.out.println(cnt);
             }
             else {
                 cnt++;
             }
         }
 
-        for(int i=0;i<list.size();i++) {
-            System.out.println(list.get(i));
-        }
     }
 }
