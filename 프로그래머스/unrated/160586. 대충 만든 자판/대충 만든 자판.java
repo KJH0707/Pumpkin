cb5304500a671347f2728817806b06d71095271e
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2023-05-31 17:15:13 +0900

    [unrated] Title: 대충 만든 자판, Time: 2.29 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        int[] answer = new int[targets.length];
+        for (int i=0;i<keymap.length;i++) {
+            String a = keymap[i];
+            for (int j=0;j<a.length();j++) {
+
+                if(map.get(a.charAt(j))==null) map.put(a.charAt(j), j + 1);
+                else {
+                    if (map.get(a.charAt(j)) > j + 1) {
+                        map.put(a.charAt(j), j + 1);
+                    }
+                }
+            }
+        }
+        for (int i=0;i<targets.length;i++) {
+            String a = targets[i];
+            int b = 0;
+            for (int j=0;j<a.length();j++) {
+                if (map.get(a.charAt(j))==null) {
+                    b=-1;
+                    break;
+                }
+                b+=map.get(a.charAt(j));
+            }
+            answer[i]=b;
+        }
+        return answer;
+    }
+}
