b2adddb49df124f06e58791e351cfb60a02f26ac
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2023-05-31 17:33:05 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.55 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,31 +1,28 @@
 import java.util.*;
 
-class Solution {
-    public int[] solution(String[] keymap, String[] targets) {
-        Map<Character,Integer> map = new HashMap<Character,Integer>();
-        int[] answer = new int[targets.length];
-        for (String a : keymap) {
-            for (int j=0;j<a.length();j++) {
-                if(map.get(a.charAt(j))==null) map.put(a.charAt(j), j + 1);
-                else {
-                    if (map.get(a.charAt(j)) > j + 1) {
-                        map.put(a.charAt(j), j + 1);
+    class Solution {
+        public int[] solution(String[] keymap, String[] targets) {
+            int[] minTouch = new int['Z' - 'A' + 1];
+            Arrays.fill(minTouch, 200);
+            for (String key : keymap) {
+                for (int i = 0; i < key.length(); i++) {
+                    minTouch[key.charAt(i) - 'A'] = Math.min(minTouch[key.charAt(i) - 'A'], i + 1);
+                }
+            }
+            int[] answer = new int[targets.length];
+            for (int i = 0; i < targets.length; i++) {
+                String target = targets[i];
+                int sum = 0;
+                boolean canMake = true;
+                for (int j = 0; j < target.length(); j++) {
+                    if (minTouch[target.charAt(j) - 'A'] == 200) {
+                        canMake = false;
+                        break;
                     }
+                    sum += minTouch[target.charAt(j) - 'A'];
                 }
+                answer[i] = canMake ? sum : -1;
             }
+            return answer;
         }
-        for (int i=0;i<targets.length;i++) {
-            String a = targets[i];
-            int b = 0;
-            for (int j=0;j<a.length();j++) {
-                if (map.get(a.charAt(j))==null) {
-                    b=-1;
-                    break;
-                }
-                b+=map.get(a.charAt(j));
-            }
-            answer[i]=b;
-        }
-        return answer;
     }
-}
