8a05822c9b100842b4b7405fec2615b8a977c4f4
KJH0707 <67087560+KJH0707@users.noreply.github.com>
2023-06-02 13:14:26 +0900

    [unrated] Title: 둘만의 암호, Time: 0.20 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,16 +1,25 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        Set<Character> set = new HashSet<>();
 
-        for (int i=0;i<s.length();i++) {
-            char b = s.charAt(i);
-            for(int j=0;j<index;j++) {
-                b++;
-                if (b>122) b=97;
-                if (skip.contains(String.valueOf(b))) j--;
-            }
-            answer+=b;
+        for (char c : skip.toCharArray()) set.add(c);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char i='a';i<='z';i++) {
+            if(!set.contains(i)) sb.append(i);
         }
-        return answer;
+
+        String a = sb.toString().repeat(3);
+        
+        for (int i=0;i<s.length();i++) {
+            char cur = s.charAt(i);
+
+            answer.append(a.charAt(a.indexOf(cur)+index));
+        }
+        return answer.toString();
     }
 }
